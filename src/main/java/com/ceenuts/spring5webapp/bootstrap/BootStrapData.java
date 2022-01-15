b8c9021f256832fc12b535c82db3014a65eac022
Damilare Durojaye <damilare.durojaye@nordsec.com>
2022-01-15 03:19:14 +0100

    Added publisher entity and created one to many relationahip with book entity

--- src/main/java/com/ceenuts/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/com/ceenuts/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package com.ceenuts.spring5webapp.bootstrap;
 
 import com.ceenuts.spring5webapp.domain.Author;
 import com.ceenuts.spring5webapp.domain.Book;
+import com.ceenuts.spring5webapp.domain.Publisher;
 import com.ceenuts.spring5webapp.repositories.AuthorRepository;
 import com.ceenuts.spring5webapp.repositories.BookRepository;
+import com.ceenuts.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,22 +14,37 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        Publisher publisher = new Publisher();
+        publisher.setName("Macmillan Publishers");
+        publisher.setCity("St. Petersburg");
+        publisher.setState("Fl");
+        publisher.setZip("14199");
+
+        publisherRepository.save(publisher);
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design", "123456");
 
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(publisher);
+        publisher.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(publisher);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "4321345");
@@ -35,10 +52,17 @@ public class BootStrapData implements CommandLineRunner {
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
 
         System.out.println("Started in Bootstrap");
+
+        System.out.println("Publisher Count: " + publisherRepository.count());
         System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
     }
 }
